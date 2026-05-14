13ad0c3ef8e9f90d17fceeded3c947c201d555e5
teachersedef81 <hatem_bozyoruk19@hotmail.com>
2026-05-14 19:58:49 +0300

    fix: use process.env.PORT for Render deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
